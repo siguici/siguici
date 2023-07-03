926947b703d0d973c136e6ee91abe7a0f1a9df94
SIGUI Kessé Emmanuel <emmanuel@kesse.sigui.ci>
2023-07-03 16:56:18 +0100

    :lipstick: Improve button display

--- src/components/contents/home.tsx
+++ src/components/contents/home.tsx
@@ -12,11 +12,11 @@ export default component$(() => {
         <p class="py-6">
           Sigui is a <strong>web application developer</strong> and <strong>web server administrator</strong>.
         </p>
-        <div class="space-x-8 mt-6">
-          <a href="https://twitter.com/siguici" target="_blank" rel="noreferrer" class="rounded py-4 px-8 bg-blue-300 ring hover:bg-blue-400 focus:bg-blue-600 ring-blue-500">
+        <div class={styles.buttons}>
+          <a href="https://twitter.com/siguici" target="_blank" rel="noreferrer" class={[styles.button, styles['button-primary']]}>
             Follow Sigui
           </a>
-          <a href="https://github.com/siguici" target="_blank" rel="noreferrer" class="rounded py-4 px-8 bg-violet-300 ring hover:bg-violet-400 focus:bg-violet-600 ring-violet-500">
+          <a href="https://github.com/siguici" target="_blank" rel="noreferrer" class={[styles.button, styles['button-secondary']]}>
             Ping Sigui
           </a>
         </div>
