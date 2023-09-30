1dbd96431db2b86c25e56976c7636c75c9c0ac69
SIGUI Kessé Emmanuel <emmanuel@kesse.sigui.ci>
2023-09-30 12:14:02 +0100

    :wrench: Use environment variables

--- astro.config.ts
+++ astro.config.ts
@@ -1,11 +1,14 @@
 import deno from '@astrojs/deno';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
+import { loadEnv } from "vite";
+
+const { SERVER_PORT } = loadEnv(process.env.NODE_ENV || 'local', process.cwd(), '');
 
 export default defineConfig({
   output: 'server',
   adapter: deno({
-    port: 3000,
+    port: Number(SERVER_PORT || 4444),
   }),
   integrations: [
     tailwind({
