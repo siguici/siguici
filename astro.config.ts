498ceb34fff0be622fef52f40a8ff984188a974c
SIGUI Kessé Emmanuel <emmanuel@kesse.sigui.ci>
2023-09-30 12:18:19 +0100

    :green_heart: Fix code style

--- astro.config.ts
+++ astro.config.ts
@@ -1,14 +1,18 @@
 import deno from '@astrojs/deno';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
-import { loadEnv } from "vite";
+import { loadEnv } from 'vite';
 
-const { SERVER_PORT } = loadEnv(process.env.NODE_ENV || 'local', process.cwd(), '');
+const { SERVER_PORT } = loadEnv(
+  process.env.NODE_ENV || 'local',
+  process.cwd(),
+  '',
+);
 
 export default defineConfig({
   output: 'server',
   adapter: deno({
-    port: Number(SERVER_PORT || 4444),
+    port: Number(SERVER_PORT || '4444'),
   }),
   integrations: [
     tailwind({
