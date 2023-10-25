a2a34601302955cd2f777c2afba6881cb6224ba6
SIGUI Kessé Emmanuel <emmanuel@kesse.sigui.ci>
2023-10-25 04:18:22 +0100

    :zap: Make robots.txt dynamic

---
+++ app/pages/robots.txt.ts
@@ -0,0 +1,16 @@
+import type { APIRoute } from 'astro';
+
+const robots = `
+# www.robotstxt.org/
+
+User-agent: *
+
+Disallow:
+
+Sitemap: ${new URL('sitemap.xml', 'https://sigui.ci').href}
+`.trim();
+
+export const GET: APIRoute = () =>
+  new Response(robots, {
+    headers: { 'Content-Type': 'text/plain' },
+  });
