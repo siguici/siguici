28d570e078fd86508e3225a9738914bde49b82d2
SIGUI Kessé Emmanuel <emmanuel@kesse.sigui.ci>
2023-10-26 17:23:37 +0100

    :tada: Start using Fresh

--- app/start.ts
+++ app/start.ts
@@ -1,25 +1,12 @@
-import { serveDir } from 'https://deno.land/std@0.140.0/http/file_server.ts';
-import { handle } from '../dist/server/entry.mjs';
+/// <reference no-default-lib="true" />
+/// <reference lib="dom" />
+/// <reference lib="dom.iterable" />
+/// <reference lib="dom.asynciterable" />
+/// <reference lib="deno.ns" />
 
-const port = Deno.env.get('SERVER_PORT') || 8000;
-const ac = new AbortController();
+import '$std/dotenv/load.ts';
+import { start } from '$fresh/server.ts';
+import manifest from './fresh.gen.ts';
+import config from '../fresh.config.ts';
 
-Deno.serve({
-  port,
-  signal: ac.signal,
-  onListen({ port, hostname }) {
-    console.log(`Server started at http://${hostname}:${port}`);
-  },
-  handler: async (req: Request) => {
-    const pathname = new URL(req.url).pathname;
-    if (
-      pathname.startsWith('/favicon.') || pathname.startsWith('/_astro') ||
-      pathname.startsWith('/assets') || pathname.startsWith('/static')
-    ) {
-      return await serveDir(req, {
-        fsRoot: 'dist/client',
-      });
-    }
-    return handle(req);
-  },
-});
+await start(manifest, config);
---
+++ fresh.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from '$fresh/server.ts';
+import twindPlugin from '$fresh/plugins/twind.ts';
+import twindConfig from './twind.config.ts';
+
+export default defineConfig({
+  plugins: [twindPlugin(twindConfig)],
+  staticDir: './out/client',
+});
