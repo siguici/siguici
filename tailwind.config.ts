f5123a06c4b0ab20eea306aac4db4e6106ae9c50
SIGUI Kessé Emmanuel <emmanuel@kesse.sigui.ci>
2023-11-10 05:36:33 +0100

    :rotating_light: Fix code style

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from "tailwindcss";
 import defaultTheme from "tailwindcss/defaultTheme";
 
 export const TailwindConfig: Config = {
-  darkMode: ['class', '.theme-dark'],
+  darkMode: ["class", ".theme-dark"],
   content: ["./app/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
