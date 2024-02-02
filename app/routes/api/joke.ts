5330592c6127da2605082c229f5afd821048c627
Sigui Kessé Emmanuel <contact@sigui.ci>
2024-02-02 15:54:52 +0100

    :arrow_up: Upgrade dependencies

--- app/routes/api/joke.ts
+++ app/routes/api/joke.ts
@@ -2,12 +2,12 @@ import { HandlerContext } from '$fresh/server.ts';
 
 // Jokes courtesy of https://punsandoneliners.com/randomness/programmer-jokes/
 const JOKES = [
-  'Why do Java developers often wear glasses? They can\'t C#.',
+  "Why do Java developers often wear glasses? They can't C#.",
   'A SQL query walks into a bar, goes up to two tables and says “can I join you?”',
-  'Wasn\'t hard to crack Forrest Gump\'s password. 1forrest1.',
-  'I love pressing the F5 key. It\'s refreshing.',
+  "Wasn't hard to crack Forrest Gump's password. 1forrest1.",
+  "I love pressing the F5 key. It's refreshing.",
   'Called IT support and a chap from Australia came to fix my network connection.  I asked “Do you come from a LAN down under?”',
-  'There are 10 types of people in the world. Those who understand binary and those who don\'t.',
+  "There are 10 types of people in the world. Those who understand binary and those who don't.",
   'Why are assembly programmers often wet? They work below C level.',
   'My favourite computer based band is the Black IPs.',
   'What programme do you use to predict the music tastes of former US presidential candidates? An Al Gore Rhythm.',
