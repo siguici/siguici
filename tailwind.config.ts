bd46a27d7d577773fd49fd8d79975c5e61dae778
Sigui Kessé Emmanuel <contact@sigui.ci>
2024-02-03 12:56:50 +0100

    :bug: Fix content path

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,7 @@ import defaultTheme from "tailwindcss/defaultTheme";
 
 export const TailwindConfig: Config = {
   darkMode: ["class", ".theme-dark"],
-  content: ["./app/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: ["./{app,src}/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
       sans: [...defaultTheme.fontFamily.sans],
