320cbfd83d90ef55e58737477bf2e633ae40ddcf
Sigui Kessé Emmanuel <contact@sigui.ci>
2024-02-04 14:27:24 +0100

    :pencil2: Fix the Sigui logo

--- app/routes/_404.tsx
+++ app/routes/_404.tsx
@@ -10,10 +10,10 @@ export default function Error404() {
         <div class='max-w-screen-md mx-auto flex flex-col items-center justify-center'>
           <img
             class='my-6'
-            src='/logo.svg'
+            src='/favicon.svg'
             width='128'
             height='128'
-            alt='the Fresh logo: a sliced lemon dripping with juice'
+            alt='The Sigui Logo'
           />
           <h1 class='text-4xl font-bold'>404 - Page not found</h1>
           <p class='my-4'>The page you were looking for doesn't exist.</p>
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -11,7 +11,7 @@ export default function Home() {
           src='/favicon.svg'
           width='128'
           height='128'
-          alt='the Fresh logo: a sliced lemon dripping with juice'
+          alt='The Sigui Logo'
         />
         <h1 class='text-4xl font-bold'>Welcome to Sigui's</h1>
         <p class='my-4'>
