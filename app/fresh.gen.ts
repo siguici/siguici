f47f4a22cd5ba3535603c6454f5756fcfa8395b6
Sigui Kessé Emmanuel <siguici@proton.me>
2024-10-15 02:25:13 +0100

    🔀 Merge the @siguici website

--- app/fresh.gen.ts
+++ app/fresh.gen.ts
@@ -2,27 +2,28 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import * as $0 from './routes/_404.tsx';
-import * as $1 from './routes/_app.tsx';
-import * as $2 from './routes/_middleware.ts';
-import * as $3 from './routes/api/joke.ts';
-import * as $4 from './routes/greet/[name].tsx';
-import * as $5 from './routes/home.tsx';
-import * as $$0 from './islands/Counter.tsx';
+import * as $_404 from './routes/_404.tsx';
+import * as $_app from './routes/_app.tsx';
+import * as $_middleware from './routes/_middleware.ts';
+import * as $api_joke from './routes/api/joke.ts';
+import * as $greet_name_ from './routes/greet/[name].tsx';
+import * as $home from './routes/home.tsx';
+import * as $Counter from './islands/Counter.tsx';
+import { type Manifest } from '$fresh/server.ts';
 
 const manifest = {
   routes: {
-    './routes/_404.tsx': $0,
-    './routes/_app.tsx': $1,
-    './routes/_middleware.ts': $2,
-    './routes/api/joke.ts': $3,
-    './routes/greet/[name].tsx': $4,
-    './routes/home.tsx': $5,
+    './routes/_404.tsx': $_404,
+    './routes/_app.tsx': $_app,
+    './routes/_middleware.ts': $_middleware,
+    './routes/api/joke.ts': $api_joke,
+    './routes/greet/[name].tsx': $greet_name_,
+    './routes/home.tsx': $home,
   },
   islands: {
-    './islands/Counter.tsx': $$0,
+    './islands/Counter.tsx': $Counter,
   },
   baseUrl: import.meta.url,
-};
+} satisfies Manifest;
 
 export default manifest;
--- src/config/app.ts
+++ src/config/app.ts
@@ -1,8 +1,8 @@
 export const AppConfig = {
-  name: 'Sigui',
-  locale: 'en-US',
-  title: 'Sigui',
-  description: 'The Sigui website.',
-  website: 'https://sigui.ci/',
-  sitemap: 'sitemap-index.xml',
+  name: "Sigui",
+  locale: "en-US",
+  title: "Sigui",
+  description: "The Sigui website.",
+  website: "https://sigui.ci/",
+  sitemap: "sitemap-index.xml",
 };
