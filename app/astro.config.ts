7d414830f6679c2bff46263a456c6ed4ad941069
⚡ Sigui 🇨🇮 <siguici@proton.me>
2026-07-02 03:30:03 +0200

    🚨 Fix code format and lint using Biome

--- app/astro.config.ts
+++ app/astro.config.ts
@@ -7,35 +7,35 @@ import { app } from './src/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: app.url,
-  srcDir: './src',
-  outDir: './out',
-  publicDir: './public',
-  compressHTML: true,
-  output: 'server',
-  image: {
-    service: passthroughImageService(),
-  },
-  adapter: deno() as AstroIntegration,
-  integrations: [
-    sitemap({
-      customPages: ['https://sikessem.com/sitemap.xml'],
-      changefreq: 'daily',
-      priority: 0.8,
-      lastmod: new Date(),
-      i18n: {
-        defaultLocale: 'en',
-        locales: {
-          en: 'en-US',
-          fr: 'fr-CI',
-        },
-      },
-    }),
-  ],
-  vite: {
-    ssr: {
-      noExternal: ['path-to-regexp'],
-    },
-    plugins: [tailwindcss()],
-  },
+	site: app.url,
+	srcDir: './src',
+	outDir: './out',
+	publicDir: './public',
+	compressHTML: true,
+	output: 'server',
+	image: {
+		service: passthroughImageService(),
+	},
+	adapter: deno() as AstroIntegration,
+	integrations: [
+		sitemap({
+			customPages: ['https://sikessem.com/sitemap.xml'],
+			changefreq: 'daily',
+			priority: 0.8,
+			lastmod: new Date(),
+			i18n: {
+				defaultLocale: 'en',
+				locales: {
+					en: 'en-US',
+					fr: 'fr-CI',
+				},
+			},
+		}),
+	],
+	vite: {
+		ssr: {
+			noExternal: ['path-to-regexp'],
+		},
+		plugins: [tailwindcss()],
+	},
 });
