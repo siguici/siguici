68537602c07daeb680a3e630d31b7f25105427c7
⚡ Sigui 🇨🇮 <siguici@proton.me>
2026-07-09 20:37:14 +0200

    🚚 Restructure monorepo and migrate tooling

--- apps/backend/routes/greet/[name].tsx
+++
@@ -1,9 +0,0 @@
-import { define } from '@/utils.ts';
-
-export const handler = define.handlers({
-  GET(ctx) {
-    return new Response(
-      `<div>Hello ${ctx.params.name}</div>`,
-    );
-  },
-});
