a4a8cc56434d2594ced4e712544ff4df73350f10
⚡ Sigui 🇨🇮 <siguici@proton.me>
2026-07-11 14:18:55 +0200

    🚚 Copy the app files to the doc

--- doc/astro.config.ts
+++ doc/astro.config.ts
@@ -1,12 +1,40 @@
+import sitemap from '@astrojs/sitemap';
 import starlight from '@astrojs/starlight';
-import { defineConfig } from 'astro/config';
+import deno from '@deno/astro-adapter';
+import tailwindcss from '@tailwindcss/vite';
+import type { AstroIntegration } from 'astro';
+import { defineConfig, passthroughImageService } from 'astro/config';
+import { app } from './src/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: app.url,
+  srcDir: './src',
+  outDir: './out',
+  publicDir: './public',
+  compressHTML: true,
+  output: 'server',
+  image: {
+    service: passthroughImageService(),
+  },
+  adapter: deno() as AstroIntegration,
   legacy: {
     collectionsBackwardsCompat: true,
   },
   integrations: [
+    sitemap({
+      customPages: ['https://sikessem.com/sitemap.xml'],
+      changefreq: 'daily',
+      priority: 0.8,
+      lastmod: new Date(),
+      i18n: {
+        defaultLocale: 'en',
+        locales: {
+          en: 'en-US',
+          fr: 'fr-CI',
+        },
+      },
+    }),
     starlight({
       favicon: '/favicon.svg',
       logo: {
@@ -55,4 +83,10 @@ export default defineConfig({
       ],
     }),
   ],
+  vite: {
+    ssr: {
+      noExternal: ['path-to-regexp'],
+    },
+    plugins: [tailwindcss()],
+  },
 });
