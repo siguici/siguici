047200d7dda893eec98e9a0a40a180e34faa0b64
⚡ Sigui 🇨🇮 <siguici@proton.me>
2026-07-11 14:26:44 +0200

    🔥 Delete the app

--- app/src/pages/feed.xml.ts
+++
@@ -1,7 +0,0 @@
-import rss from '@astrojs/rss';
-import type { APIContext } from 'astro';
-import { rssWork } from '../utils';
-
-export async function GET(context: APIContext) {
-  return rss(await rssWork(context));
-}
