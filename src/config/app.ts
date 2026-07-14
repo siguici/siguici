4ba4c28d6055723c8557b7a8b31c50d92c3a9666
⚡ Sigui 🇨🇮 <siguici@proton.me>
2026-07-14 17:15:08 +0200

    🔀 chore: convert repository into GitHub profile

--- src/config/app.ts
+++
@@ -1,8 +0,0 @@
-export const app = {
-  name: 'SKE',
-  locale: 'en-US',
-  title: 'Sigui Kessé Emmanuel',
-  description: 'Sigui Kessé Emmanuel’s website.',
-  url: 'https://ske.deno.dev/',
-  sitemap: 'sitemap-index.xml',
-};
